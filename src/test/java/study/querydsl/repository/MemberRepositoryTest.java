580c728bb353254df708fa65291e93a8f3eac4ad
JinDalgi <dalgi5986@gmail.com>
2023-11-27 03:36:32 +0900

    querydsl Web 한계점

--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -131,6 +131,14 @@ class MemberRepositoryTest {
             System.out.println("member1 = " + member);
         }
 
+        /*
+        * Querydsl Web 지원
+        * 한계점
+        * - 단순한 조건만 가능
+        * - 조건을 커스텀하는 기능이 복잡하고 명시적이지 않음
+        * - 컨트롤러가 Querydsl에 의존
+        * - 복잡한 실무환경에서 사용하기에는 한계가 명확
+        * */
     }
 
 }
